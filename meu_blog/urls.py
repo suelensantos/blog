592478bbfe1e6edf8631e3c1f222e86dec028bba
Suelen Santos <suelensantos.cordeiro@gmail.com>
2016-09-02 17:39:17 -0300

    Nova URL Foi feita uma nova url com essa expressão: http://localhost:8000/meu_blog/texto-qualquer/02/09/2016/artigo.html/

--- meu_blog/urls.py
+++ meu_blog/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import url
 from . import views 
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'), 
+    url(r'^$', views.index, name='index'),
+    url(r'^(?P<titulo>\w+)/(?P<dia>\d{2})/(?P<mes>\d{2})/(?P<ano>\d{4})/artigo\.html/$', views.viewArtigo, name='artigo'),
 ]
--- meu_blog/views.py
+++ meu_blog/views.py
@@ -1,4 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals # acrescentar para corrigir o erro de UnicodeDecodeError
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Artigo 
 
 # Create your views here.
@@ -6,3 +11,36 @@ def index(request):
 	latest = Artigo.objects.all() 
 	context = {'latest': latest}
 	return render(request, 'meu_blog/index.html', context)
+
+def viewArtigo(resquest, titulo, dia, mes, ano):
+
+	# Validação do dia ---->> Lembrar que não é referente se é número, caracter, mas verificar a partir da ideia de uma data..
+
+	# tipo, dia só pode ser de 1 até 31
+	# mes só pode de 1 até 12
+	# e pro ano é preciso pensar numa exceção
+	# pro título, restringe o tamanho dele.. no models.py, só vai até 100 caracteres.
+
+	# Validação do mês 
+
+	# Validação do ano
+
+	text = "Título '%s', datada de %s/%s/%s." %(titulo, dia, mes, ano)
+	return HttpResponse(text)
+
+def valida_titulo(titulo):
+
+	return 
+
+def valida_dia(dia):
+
+	return 
+
+def valida_mes(mes):
+
+	return
+
+def valida_ano(ano):
+
+	return
+
