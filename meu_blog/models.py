4072f3eeea7fdc49a81522754d844fbd78e9f2f4
Suelen Santos <suelensantos.cordeiro@gmail.com>
2016-09-02 15:14:19 -0300

    Criação da classe Meta

--- meu_blog/models.py
+++ meu_blog/models.py
@@ -1,18 +1,26 @@
 from __future__ import unicode_literals
-import datetime
 from django.db import models
+import datetime
 from django.utils import timezone
 
 # Create your models here.
 class Artigo(models.Model):
-   titulo = models.CharField(max_length=100)
-   conteudo = models.TextField()
-   publicacao = models.DateTimeField()
 
-   def foi_publicado_recentemente(self):
-	now = timezone.now()
-	return now - datetime.timedelta(days=1) <= self.publicacao <= now
+    class Meta:
+    	# o '-' na frente de publicacao eh usado para colocar a lista em ordem decrescente 
+        ordering = ('-publicacao',) 
 
-   foi_publicado_recentemente.admin_order_field = 'publicacao'
-   foi_publicado_recentemente.boolean = True
-   foi_publicado_recentemente.short_description = 'Publicado recentemente?'
+    titulo = models.CharField(max_length=100)
+    conteudo = models.TextField()
+    publicacao = models.DateTimeField(
+   		default = datetime.datetime.now(),
+   		blank = True
+   		)
+
+    def foi_publicado_recentemente(self):
+		now = timezone.now()
+		return now - datetime.timedelta(days=1) <= self.publicacao <= now
+
+    foi_publicado_recentemente.admin_order_field = 'publicacao'
+    foi_publicado_recentemente.boolean = True
+    foi_publicado_recentemente.short_description = 'Publicado recentemente?'
